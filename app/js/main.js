847fd2ddebd0f37261158e5511a344e0c9a48df6
Viktor Dirin <vdirin@gmail.com>
2024-01-19 02:58:41 +0400

    Added Footer, problem on main.js with .rateYo

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,9 @@ $(function () {
 
     $(".feedback__info-stars, .feedback__cards-item-stars").rateYo({
         starWidth: "20px",
-        spacing: "4px"
+        spacing: "12px",
+        // normalFill: "#969aa0",
+        // starSvg: '<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><circle cx="11.998" cy="11.998" fill-rule="nonzero" r="9.998"/></svg>'
     });
 });
 
